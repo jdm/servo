e90e5703fb9f1cdb71d06446e3018a150f36c269
Josh Matthews <josh@joshmatthews.net>
2020-03-23 12:21:07 -0400

    tmp

--- support/hololens/ServoApp/DefaultUrl.h
+++ support/hololens/ServoApp/DefaultUrl.h
@@ -1,5 +1,5 @@
 #pragma once
 
 //#define DEFAULT_URL L"https://joshmatthews.net/servo.org/hl-home/"
-#define DEFAULT_URL L"https://joshmatthews.net/servo.org/hl-home/bbjs/#8Y780Y#57"
+#define DEFAULT_URL L"https://www.joshmatthews.net/servo.org/hl-home/bbjs/#8Y780Y#57"
 //#define DEFAULT_URL L"https://joshmatthews.net/mul.html"
