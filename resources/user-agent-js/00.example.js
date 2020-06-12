68056f8a68ef151414afe0449865545fd0ef4780
Josh Matthews <josh@joshmatthews.net>
2020-06-12 15:55:13 -0400

    helpful userscript stubs

--- resources/user-agent-js/00.example.js
+++ resources/user-agent-js/00.example.js
@@ -3,3 +3,17 @@
 // Files are sorted alphabetically. When committing polyfills
 // order them with numbers, e.g. `01.innerhtml.js` will be executed before
 // `05.jquery.js`
+onunhandledrejection = function(e) { console.error("XXX promise rejection: " + e.reason); }
+window.RTCDataChannelEvent = {};
+window.RTCPeerConnection.prototype.createDataChannel = function() {
+    return {
+        addEventListener: function() {},
+        removeEventListener: function() {},
+        readyState: "closed",
+    };
+}
+window.console.group = function() {}
+window.console.groupEnd = function() {}
+window.MediaDevices.prototype.enumerateDevices = function() {
+    return Promise.resolve([]);
+};
